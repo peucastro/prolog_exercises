dfadb98856b566014d6e370c5ae1008f51f33c2d
Pedro Castro <falecompedroac@gmail.com>
2025-12-06 00:03:41 +0000

    sheet2: finished ex02

--- sheet1/ex02.pl
+++ sheet1/ex02.pl
@@ -57,3 +57,27 @@ attends(dalmindo, _X).
 % Is Eduarda a student of Bernardete?
 attends(eduarda, _X), teaches(bernardete, _X).
 */
+
+/* c) Write rules in Prolog that allow answering the following questions: */
+
+% Is X a student of professor Y?
+% Who are the students of professor X?
+% Who are the teachers of student X?
+student(X, Y) :- attends(X, _S), teaches(Y, _S).
+
+% Who is a student of both professor X and professor Y?
+student_of_both(Z, X, Y) :- teaches(X, S1),
+                            teaches(Y, S2),
+                            attends(Z, S1),
+                            attends(Z, S2),
+                            X \= Y,
+                            S1 \= S2.
+
+% Who is colleagues with whom? (two students are colleagues if they attend at least one course in common; two teachers are colleagues)
+colleagues(X, Y) :- teaches(X, _), teaches(Y, _), X \= Y.
+colleagues(X, Y) :- attends(X, _S), attends(Y, _S), X \= Y.
+
+% Who are the students that attend more than one course?
+attends_more_than_1_course(X) :- attends(X, C1),
+                                 attends(X, C2),
+                                 C1 \= C2.
