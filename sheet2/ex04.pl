2445a2fd98aa5276b71a632a79ca293697d350e4
Pedro Castro <falecompedroac@gmail.com>
2025-12-09 17:26:04 +0000

    sheet3: ex04

--- sheet2/ex04.pl
+++ sheet2/ex04.pl
@@ -14,6 +14,25 @@ sum_rec(N, Sum) :- N > 1,
                    sum_rec(N1, Sum1),
                    Sum is N + Sum1.
 
+% c) Implement the pow_rec(+X, +Y, -P) predicate, which recursively determines the result of raising X to the power of Y.
+
+pow_rec(_, 0, 1).
+pow_rec(X, Y, P) :- Y > 0,
+                    Y1 is Y - 1,
+                    pow_rec(X, Y1, P1),
+                    P is P1 * X.
+
+% d) Implement the square_rec(+N, -S) predicate, which recursively determines que square of a number N (ie, without using multiplications).
+% Suggestion: you may need to use an auxiliary predicate with additional arguments.
+
+aux_s(_, 0, 0).
+aux_s(X, C, S) :- C > 0,
+                C1 is C - 1,
+                aux(X, C1, S1),
+                S is S1 + X.
+
+square_rec(N, S) :- aux_s(N, N, S).
+
 % e) Implement the fibonacci(+N, -F) predicate, which determines the Fibonacci number of order N.
 
 fib(0, 0).
@@ -25,3 +44,32 @@ fib(N, S) :- N > 2,
              fib(N1, S1),
              fib(N2, S2),
              S is S1 + S2.
+
+% f) Implement collatz(+N, -S), which receives a positive integer number N and determines the number of steps necessary to reach 1 following the operations set forth by this sequence: if N is even, in the next step it will take the value N/2; if N is odd, in the next step it will take the value 3N+1.
+
+collatz(1, 0).
+collatz(N, S) :- N > 1,
+                 0 is N mod 2,
+                 N1 is N // 2,
+                 collatz(N1, S1),
+                 S is S1 + 1.
+collatz(N, S) :- N > 1,
+                 1 is N mod 2,
+                 N1 is 3 * N + 1,
+                 collatz(N1, S1),
+                 S is S1 + 1.
+
+% g) Implement the is_prime(+X) predicate, which determines whether X is a prime number. Suggestion: a number is prime if it is divisible only by itself and one.
+
+aux_p(N, I) :- I * I > N.
+aux_p(N, I) :- I > 0,
+               R is N mod I,
+               R \= 0,
+               I1 is I + 1,
+               aux_p(N, I1).
+
+is_prime(2).
+is_prime(X) :- X > 1,
+               aux_p(X, 2).
+               
+
