6f7d0cd12bd9c6cd6989e7f6cc3d47fae5c54ca2
Pedro Castro <falecompedroac@gmail.com>
2025-12-09 14:20:23 +0000

    sheet2: ex01

---
+++ sheet2/ex01.pl
@@ -0,0 +1,30 @@
+r(a, b).
+r(a, d).
+r(b, a).
+r(a, c).
+
+s(b, c).
+s(b, d).
+s(c, c).
+s(d, e).
+
+/*
+r(X, Y), s(Y, Z).
+
+a, b, c
+a, b, d
+a, d, e
+a, c, c
+*/
+
+/*
+s(Y, Y), r(X, Y).
+
+c, a
+*/
+
+/*
+r(X, Y), s(Y, Y).
+
+a, c
+*/
