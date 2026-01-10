b8bb8511b93b565fd27d2997e915d07de76c6f2c
Pedro Castro <falecompedroac@gmail.com>
2026-01-10 15:21:50 +0000

    sheet3: updated ex03

--- sheet3/ex03.pl
+++ sheet3/ex03.pl
@@ -15,18 +15,15 @@ del_one(Elem, [X | Xs], [X | List2]) :- X \= Elem,
 /* c) Implement del_all(+Elem, +List1, ?List2), which deletes all occurrences of Elem from List1, resulting in List2 */
 
 del_all(_, [], []).
-del_all(Elem, [X | Xs], List2) :- X == Elem,
-                                  del_all(Elem, Xs, List2).
+del_all(Elem, [Elem | Xs], List2) :- del_all(Elem, Xs, List2).
 del_all(Elem, [X | Xs], [X | List2]) :- X \== Elem,
                                         del_all(Elem, Xs, List2).
 
 /* d) Implement del_all_list(+ListElems, +List1, ?List2), which deletes from List1 all occurrences of all elements of ListElems, resulting in List2. */
 
-del_all_list(_, [], []).
-del_all_list(ListElems, [X | Xs], List2) :- memberchk(X, ListElems),
-                                            del_all_list(ListElems, Xs, List2).
-del_all_list(ListElems, [X | Xs], [X | List2]) :- \+ memberchk(X, ListElems),
-                                                  del_all_list(ListElems, Xs, List2).
+del_all_list([], List1, List1).
+del_all_list([Target | ListElems], List1, List2) :- del_all(Target, List1, TempList),
+                                                    del_all_list(ListElems, TempList, List2).
 
 /* e) Implement del_dups(+List1, ?List2), which eliminates repeated values from List1. */
 
@@ -49,7 +46,7 @@ replicate(Amount, Elem, [Elem | List]) :- Amount > 0,
 
 intersperse(_, [], []).
 intersperse(_, [X], [X]) :- !.
-intersperse(Elem, [X | Xs], [X | [Elem | List2]]) :- intersperse(Elem, Xs, List2).
+intersperse(Elem, [X | Xs], [X, Elem | List2]) :- intersperse(Elem, Xs, List2).
 
 /* i) Implement insert_elem(+Index, +List1, +Elem, ?List2), which inserts Elem into List1 at position Index, resulting in List2. */
 
@@ -63,8 +60,8 @@ insert_elem(Index, [X | Xs], Elem, [X | List2]) :- Index > 0,
 
 delete_elem(0, [X | Xs], X, Xs).
 delete_elem(Index, [X | Xs], Elem, [X | List2]) :- Index > 0,
-                                             Index1 is Index - 1,
-                                             delete_elem(Index1, Xs, Elem, List2).
+                                                   Index1 is Index - 1,
+                                                   delete_elem(Index1, Xs, Elem, List2).
 
 /* k) Implement replace(+List1, +Index, ?Old, +New, ?List2), which replaces the Old element, located at position Index in List1, by New, resulting in List2. */
 
