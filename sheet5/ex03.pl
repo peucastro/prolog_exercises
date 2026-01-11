ffcd18055d334f7a0fc74021d01f00f1591ba3d0
Pedro Castro <falecompedroac@gmail.com>
2026-01-11 14:46:59 +0000

    sheet5: ex03

--- sheet5/ex03.pl
+++ sheet5/ex03.pl
@@ -28,3 +28,50 @@ short_classes(L) :- findall(UC-Day/Time, (class(UC, _, Day, Time, Duration), Dur
 /* d) Implement course_classes(+Course, -Classes), which receives a course and returns a list of all the classes from that course (list of terms in the format Day/Time-Type). */
 
 course_classes(Course, Classes) :- findall(Day/Time-Type, (class(Course, Type, Day, Time, _)), Classes).
+
+/* e) Implement courses(-L), which returns a list of all existing courses. Avoid repeated results. */
+
+courses(L) :- setof(Course, Type^Day^Time^Duration^(class(Course, Type, Day, Time, Duration)), L).
+
+/* f) Implement schedule/0, which prints all the classes in the terminal, in the order by which they take place during the week. */
+
+print_classes([]).
+print_classes([Day-Time-Duration/Course-Type | Tail]) :- format('~w ~wh (~w hrs): ~w (~w)~n', [Day, Time, Duration, Course, Type]),
+                                                         print_classes(Tail).
+
+schedule :- setof(Day-Time-Duration/Course-Type, class(Course, Type, Day, Time, Duration), Schedule),
+            print_classes(Schedule).
+
+/* g) Modify the previous predicates so that the day of the week is printed only as Mon, Tue, Wed, Thu or Fri.
+Tip: use a 'translation' predicate to convert between the internal representation format and the display format. */
+
+clean_day('1 Mon', 'Mon').
+clean_day('2 Tue', 'Tue').
+clean_day('3 Wed', 'Wed').
+clean_day('4 Thu', 'Thu').
+clean_day('5 Fri', 'Fri').
+
+print_classes_clean([]).
+print_classes_clean([Day-Time-Duration/Course-Type | Tail]) :- clean_day(Day, CleanDay),
+                                                               format('~w ~wh (~w hrs): ~w (~w)~n', [CleanDay, Time, Duration, Course, Type]),
+                                                               print_classes_clean(Tail).
+
+schedule_clean :- setof(Day-Time-Duration/Course-Type, class(Course, Type, Day, Time, Duration), Schedule),
+                  print_classes_clean(Schedule).
+
+/* h) Implement find_class/0, which asks the user for a day and time, and indicates whether a class starts or is taking place at that time, printing the class, start time and duration.
+If no class is taking place, the predicate should display a message stating that. */
+
+find_class :- write('Day: '), read(InputDay),
+              write('Time: '), read(QueryTime),
+              (
+                clean_day(InternalDay, InputDay),
+                class(UC, Type, InternalDay, Start, Duration),
+                End is Start + Duration,
+                QueryTime >= Start,
+                QueryTime < End
+              ->
+                format('~w ~wh (~w hrs): ~w (~w)~n', [InputDay, Start, Duration, UC, Type])
+              ;
+                format('No class found on ~w at ~w~n', [InputDay, QueryTime])
+              ).
